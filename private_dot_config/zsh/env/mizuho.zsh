1d335417ecf98023d08de72a900918ccd334be14
dom1nux <go.francko.dr@gmail.com>
2025-03-19 13:02:27 -0500

    feat(zsh): add env variables

---
+++ private_dot_config/zsh/env/mizuho.zsh
@@ -0,0 +1,5 @@
+#### Docker env
+#export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+#### Helix env
+export EDITOR=helix
---
+++ private_dot_config/zsh/env/mizuki.zsh
@@ -0,0 +1,6 @@
+#### Homebrew env
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+#### Rustup env
+export PATH="/home/linuxbrew/.linuxbrew/opt/rustup/bin:$PATH"
+
