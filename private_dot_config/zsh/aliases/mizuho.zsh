590937fa89a8ce89b599ed84ae4acc7572ecce97
dom1nux <go.francko.dr@gmail.com>
2025-03-20 13:04:18 -0500

    refactor(zsh): change alias of `chezmoi` command to `cz`

--- private_dot_config/zsh/aliases/mizuho.zsh
+++ private_dot_config/zsh/aliases/mizuho.zsh
@@ -1,7 +1,7 @@
 # Aliases
 alias hx='helix'
 alias k='kubectl'
-alias chez='chezmoi'
+alias cz='chezmoi'
 alias font-status='gsettings get org.gnome.desktop.interface text-scaling-factor'
 alias font-bigger='gsettings set org.gnome.desktop.interface text-scaling-factor 1.2'
 alias font-smaller='gsettings set org.gnome.desktop.interface text-scaling-factor 1.0'
